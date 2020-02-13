98975ffeb9dd1928d08d0a9aecf3349719397860
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-13 23:50:53 +0300

    Add method checkDataFromFile to Validator class

--- src/main/java/by/epam/javatraining/matrix/validator/Validator.java
+++ src/main/java/by/epam/javatraining/matrix/validator/Validator.java
@@ -3,6 +3,30 @@ package by.epam.javatraining.matrix.validator;
 public class Validator {
 
     public boolean checkDataFromFile(String data) {
+        if (data == null)
+            return false;
+        String[] initArray = data.split(" ");
+        if (initArray.length != 2)
+            return false;
 
+        int matrixSize;
+        try {
+            matrixSize = Integer.parseInt(initArray[0]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if (matrixSize < 1)
+            return false;
+
+        int numberOfThreads;
+        try {
+            numberOfThreads = Integer.parseInt(initArray[1]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if (numberOfThreads < matrixSize && numberOfThreads % matrixSize != 0)
+            return false;
+
+        return true;
     }
 }
