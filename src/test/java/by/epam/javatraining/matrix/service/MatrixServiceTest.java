39a64f8aa4784cdf9330b8a918317f0cff332521
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-14 20:03:46 +0300

    Add MatrixServiceTest class

--- src/test/java/by/epam/javatraining/matrix/service/MatrixServiceTest.java
+++ src/test/java/by/epam/javatraining/matrix/service/MatrixServiceTest.java
@@ -1,12 +1,41 @@
 package by.epam.javatraining.matrix.service;
 
+import by.epam.javatraining.matrix.entity.MatrixHolder;
+import by.epam.javatraining.matrix.exception.ServiceException;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MatrixServiceTest {
 
     @Test
-    public void testFindSumOfColumnAndRowElements() {
+    public void testFindSumOfColumnAndRowElements3On3Matrix() {
         MatrixService matrixService = MatrixService.getInstance();
+        MatrixHolder matrixHolder = MatrixHolder.getInstance();
 
+        int[][] matrix = {  {2, 4, 5},
+                            {5, 6, 7},
+                            {1, 2, 3}};
+        matrixHolder.setMatrix(matrix);
+
+        int diagonalPlace = 1;
+        int excepted = 30;
+
+        int actual = matrixService.findSumOfColumnAndRowElements(diagonalPlace);
+
+        Assert.assertEquals(excepted, actual);
+    }
+
+    @Test
+    public void  testInitMatrix() throws ServiceException {
+        MatrixService matrixService = MatrixService.getInstance();
+        MatrixHolder matrixHolder = MatrixHolder.getInstance();
+
+        int[][] excepted = new int[5][5];
+
+        matrixService.initMatrix();
+
+        int[][] actual = matrixHolder.getMatrix();
+
+        Assert.assertArrayEquals(excepted, actual);
     }
 }
